964eb56a2bfb2e89a9eb3c7e02115e9f374543b2
AlexisBarriera <alexisbarriera72@gmail.com>
2025-09-29 16:20:26 -0400

    Delete global.d.ts

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-interface Window {
-  persistentStorage: {
-    setItem(key: string, value: string): Promise<void>;
-    getItem(key: string): Promise<string | null>;
-    removeItem(key: string): Promise<void>;
-    clear(): Promise<void>;
-  };
-}
